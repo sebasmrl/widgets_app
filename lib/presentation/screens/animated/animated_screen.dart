504b98893bbfb6446362c0207cd61f24577326c8
Sebastian Morales <davesebastian99@gmail.com>
2024-08-30 13:11:59 -0500

    Added AnimatedScreen

--- lib/presentation/screens/animated/animated_screen.dart
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -1,9 +1,55 @@
+import 'dart:math' show Random;
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
 
   static const name = 'animated_screen';
   const AnimatedScreen({super.key});
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+
+
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+
+
+  double _width = 150;
+  double _height = 150;
+  Color _color = Colors.green;
+  double _borderRadius = 20;
+  bool _isButtonDissabled = false;
+
+  void cambiarFormaContenedor(){
+
+    setState(() {
+      _isButtonDissabled = true;
+    });
+
+    final random = Random();
+
+      // Generate a random width and height.
+      _width = random.nextInt(300).toDouble();
+      _height = random.nextInt(300).toDouble();
+
+      // Generate a random color.
+      _color = Color.fromRGBO(
+        random.nextInt(256),
+        random.nextInt(256),
+        random.nextInt(256),
+        1,
+      );
+      _borderRadius = random.nextInt(50).toDouble();
+
+
+    setState(() {
+      _isButtonDissabled = false;
+    });
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -15,16 +61,18 @@ class AnimatedScreen extends StatelessWidget {
         child: AnimatedContainer(
           duration: const Duration(milliseconds: 400),
           curve: Curves.easeOutCubic,//tipo de animacion
-          width:100, 
-          height: 100,
+          width: _height, 
+          height: _width,
           decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(20)
+            color: _color,
+            borderRadius: BorderRadius.circular(_borderRadius)
           ),
         ) ,
       ),
-      floatingActionButton: FloatingActionButton(onPressed: (){},
-      child: const Icon(Icons.play_arrow_rounded),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _isButtonDissabled ? null : cambiarFormaContenedor,
+        child: const Icon(Icons.play_arrow_rounded),
+      ),
     );
   }
 }
