d2d66709564b9fb093ecb5f480d0f36ec5b72fed
Sebastian Morales <davesebastian99@gmail.com>
2024-09-02 19:07:14 -0500

    Removed unnecessary importation

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,7 +1,6 @@
-import 'dart:ffi';
+import 'package:flutter/material.dart';
 
 import 'package:animate_do/animate_do.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class InfiniteScrollScreen extends StatefulWidget {
